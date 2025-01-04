f66d92da7f058fa7f2b941b3cdb57421a913c1b5
imvjai <vjai.chourasia@gmail.com>
2025-01-04 15:22:27 +0000

    all is working, web server is serving and nginx is now containarized

--- mywebapp/settings.py
+++ mywebapp/settings.py
@@ -147,6 +147,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'main_app/static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
